f320545d5eb750e555816a64a85a36b02727c4d1
probablyvictorian <probablyvictorian@gmail.com>
2026-03-26 15:24:57 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+// Массивы с файлами
+const logos = [
+    'pv_lg_01.svg', 'pv_lg_02.svg', 'pv_lg_03.svg', 'pv_lg_04.svg',
+    'pv_lg_05.svg', 'pv_lg_06.svg', 'pv_lg_07.svg', 'pv_lg_08.svg',
+    'pv_lg_09.svg', 'pv_lg_10.svg', 'pv_lg_11.svg', 'pv_lg_12.svg',
+    'pv_lg_13.svg', 'pv_lg_14.svg', 'pv_lg_15.svg', 'pv_lg_16.svg'
+];
+
+const backgrounds = [
+    'lg_bg_1.jpg', 'lg_bg_2.jpg', 'lg_bg_3.jpg', 'lg_bg_4.jpg'
+];
+
+const colors = [
+    '#eaeaea', '#f5f0e6', '#e8e4dc', '#f0ebe1'
+];
+
+// Функция случайного выбора
+function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+// Случайно выбираем элементы
+const randomLogo = random(logos);
+const randomBg = random(backgrounds);
+const randomTexture = random(backgrounds);
+const randomColor = random(colors);
+
+// Применяем цвет фона
+document.body.style.backgroundColor = randomColor;
+
+// Применяем фон страницы
+document.body.style.backgroundImage = `url('images/${randomBg}')`;
+
+// Загружаем SVG логотип
+fetch(`images/${randomLogo}`)
+    .then(response => response.text())
+    .then(svgContent => {
+        // Вставляем SVG в контейнер
+        const logoContainer = document.querySelector('.logo-container');
+        logoContainer.innerHTML = svgContent;
+        
+        // Добавляем паттерн текстуры
+        const svg = logoContainer.querySelector('svg');
+        const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+        const pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+        const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+        
+        pattern.setAttribute('id', 'logo-pattern');
+        pattern.setAttribute('patternUnits', 'userSpaceOnUse');
+        pattern.setAttribute('width', '500');
+        pattern.setAttribute('height', '500');
+        
+        image.setAttribute('href', `images/${randomTexture}`);
+        image.setAttribute('width', '500');
+        image.setAttribute('height', '500');
+        
+        pattern.appendChild(image);
+        defs.appendChild(pattern);
+        svg.insertBefore(defs, svg.firstChild);
+        
+        // Применяем паттерн ко всем элементам SVG
+        const paths = svg.querySelectorAll('path, rect, circle, polygon, text');
+        paths.forEach(path => {
+            path.setAttribute('fill', 'url(#logo-pattern)');
+        });
+    });

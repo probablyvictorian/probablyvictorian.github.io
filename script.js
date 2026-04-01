05ae03c80770f57ede4dd0c0ecfe03153c6520a6
probablyvictorian <probablyvictorian@gmail.com>
2026-04-01 18:43:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const logo = document.querySelector('#logo');
 const body = document.body;
 
 logo.addEventListener('mouseenter', function() {
-    body.style.backgroundImage = "url('images/bg02.gif')";
+    body.classList.add('inverted');
 });
 
 logo.addEventListener('mouseleave', function() {
-    body.style.backgroundImage = "url('images/bg01.gif')";
+    body.classList.remove('inverted');
 });
